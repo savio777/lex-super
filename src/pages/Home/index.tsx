c087d149779704534223891f48fc570f49eb39e3
Sávio Palácio Fontes <saviopf22@gmail.com>
2021-10-02 00:14:56 -0300

    select funcional

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,12 +1,13 @@
 import React, { useState } from "react";
 
-import { FlatList } from "react-native";
+import { FlatList, View } from "react-native";
 import { Picker } from "@react-native-picker/picker";
 
 import ItemProduct from "../../components/ItemProduct";
 import { IProduct, DATA, listCategories } from "../../helpers/mockupData";
+import colors from "../../helpers/colors";
 
-import { Container } from "./styles";
+import { Container, ContainerPicker } from "./styles";
 
 export default function Home() {
   const [categorieSelected, setCategorieSelected] = useState<
@@ -15,35 +16,43 @@ export default function Home() {
 
   return (
     <Container>
-      <Picker
-        selectedValue={categorieSelected}
-        onValueChange={(value) => setCategorieSelected(value)}
-      >
-        {listCategories.map((categorie) => (
-          <Picker.Item
-            key={categorie}
-            label={categorie.toLocaleUpperCase()}
-            value={categorie}
-          />
-        ))}
-      </Picker>
+      <ContainerPicker>
+        <Picker
+          selectedValue={categorieSelected}
+          onValueChange={(value) => setCategorieSelected(value)}
+          style={{
+            color: "#fff",
+            marginTop: 10,
+          }}
+        >
+          {listCategories.map((categorie) => (
+            <Picker.Item
+              key={categorie}
+              label={categorie.toLocaleUpperCase()}
+              value={categorie}
+            />
+          ))}
+        </Picker>
+      </ContainerPicker>
 
       <FlatList
         style={{ marginTop: 20 }}
         horizontal
         data={DATA}
         keyExtractor={(_, index) => String(index)}
-        renderItem={({ item }) => (
-          <ItemProduct
-            imageSource={require("../../assets/products/celular3.jpg")}
-            title={item.title}
-            onPressBuy={() => console.log(`abrir link do ${item.title}`)}
-            stars={[true, true, true, true, false]}
-            value={item.value}
-            previousValue={item.previousValue}
-            numberStores={item.numberStores}
-          />
-        )}
+        renderItem={({ item }) =>
+          item.categorie === categorieSelected ? (
+            <ItemProduct
+              imageSource={require("../../assets/products/celular3.jpg")}
+              title={item.title}
+              onPressBuy={() => console.log(`abrir link do ${item.title}`)}
+              stars={[true, true, true, true, false]}
+              value={item.value}
+              previousValue={item.previousValue}
+              numberStores={item.numberStores}
+            />
+          ) : null
+        }
       />
     </Container>
   );
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,8 +1,16 @@
 import styled from "styled-components/native";
+
 import colors from "../../helpers/colors";
 
 export const Container = styled.View`
   flex: 1;
   background-color: ${colors.tealBlue};
   padding: 5px;
+`;
+
+export const ContainerPicker = styled.View`
+  border-bottom-width: 1px;
+  border-color: #fff;
+  border-radius: 5px;
+  padding-bottom: 5px;
 `;
